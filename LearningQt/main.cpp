0fd1b0f2cbe3db76b66158aeae221bbe58192384
unknown <stanislav.v.yankov@gmail.com>
2020-10-24 14:27:49 -0400

    [Update] Set fixed size for the scene and the view. Centered player in middle of the view

--- LearningQt/main.cpp
+++ LearningQt/main.cpp
@@ -13,14 +13,14 @@ int main( int argc, char* argv[] )
 
     // Creating scene and item
     QGraphicsScene* pScene = new QGraphicsScene();
-    CRectItem* pRectItem = new CRectItem();
+    CRectItem* pPlayer = new CRectItem();
 
-    pRectItem->setRect( 0, 0, 100, 100 );
-    pScene->addItem( pRectItem );
+    pPlayer->setRect( 0, 0, 100, 100 );
+    pScene->addItem( pPlayer );
 
     // Make rect focusable and focus it
-    pRectItem->setFlag( QGraphicsItem::ItemIsFocusable );
-    pRectItem->setFocus();
+    pPlayer->setFlag( QGraphicsItem::ItemIsFocusable );
+    pPlayer->setFocus();
 
     QGraphicsView* pView = new QGraphicsView( pScene );
 
@@ -28,6 +28,9 @@ int main( int argc, char* argv[] )
     pView->setVerticalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
 
     pView->show();
+    pView->setFixedSize( 800, 600 );
+    pView->setSceneRect( 0, 0, 800, 600 );
+    pPlayer->setPos( pView->width() / 2 - pPlayer->rect().width() / 2, pView->height() - pPlayer->rect().height() - 3 );
 
     return a.exec();
 }
