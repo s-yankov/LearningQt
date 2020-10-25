c9ec651f559dbb2c80a8a466e5b55c9b9d432650
unknown <stanislav.v.yankov@gmail.com>
2020-10-25 16:21:33 +0200

    [Update] Set default width for the score text

--- LearningQt/Score.cpp
+++ LearningQt/Score.cpp
@@ -1,9 +1,15 @@
+///////////////////
+/// Score.cpp
+///
 #include "Score.h"
 
 #include <QFont>
 
+#define SCORE_TEXT_WIDTH 100
+
 CScore::CScore( QGraphicsItem* pParent ) : QGraphicsTextItem( pParent )
 {
+    setTextWidth( SCORE_TEXT_WIDTH );
     setPlainText( m_strText + QString::number( m_lScore ) );
     setDefaultTextColor( Qt::blue );
     setFont( QFont( "times", 16 ) );
